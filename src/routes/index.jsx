c71ea78729fb5d81a0d3dbfa8352851c6aac94fa
rizalsy17 <83525054+rizalsy17@users.noreply.github.com>
2024-05-27 15:48:04 +0700

    fix route
    fix route

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -23,36 +23,4 @@ const AppRoutes = () => {
         </Router>
     );
 };
-
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "../contexts/authContext";
-import ProtectedRoute from "./protectedRoute";
-import Home from "../pages/home";
-import Login from "../pages/auth/login";
-import Register from "../pages/auth/register";
-import HomeUser from "../pages/user/home";
-
-function AppRoutes() {
-  return (
-    <Router>
-      <AuthProvider>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route
-            path="/home"
-            element={
-              <ProtectedRoute>
-                <HomeUser />
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
-      </AuthProvider>
-    </Router>
-  );
-}
-
 export default AppRoutes;
