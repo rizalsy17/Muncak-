e94cb36cd1ad91b6e0631e7770778d01f939d476
Naresh <nareshpratista28@gmail.com>
2024-05-28 22:37:36 +0700

    feat: add NotFoundPage

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,5 +1,37 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import NotFoundImage from "../assets/rafiki.svg"; // Pastikan path ke gambar sudah benar
 
 export default function NotFoundPage() {
-  return <div>Not Found Page</div>;
+  const navigate = useNavigate();
+
+  const handleGoBack = () => {
+    navigate(-1); // Navigasi ke halaman sebelumnya
+  };
+
+  return (
+    <div className="flex flex-col md:flex-row items-center justify-center h-screen text-center md:text-left">
+      <div className="flex items-center justify-center mb-4 md:mb-0 md:mr-4">
+        <img
+          src={NotFoundImage}
+          alt="Page not found"
+          className="w-3/4 md:w-96"
+        />
+      </div>
+      <div>
+        <h1 className="text-3xl md:text-4xl text-darkText font-semibold mb-2">
+          This Page Could Not Be Found!
+        </h1>
+        <p className="text-sm md:text-lg text-darkText">
+          We are sorry, but the page you are looking for is not available
+        </p>
+        <button
+          onClick={handleGoBack}
+          className="px-4 py-2 mt-3 bg-primary text-white rounded-lg shadow-md transition duration-300"
+        >
+          Go back
+        </button>
+      </div>
+    </div>
+  );
 }
